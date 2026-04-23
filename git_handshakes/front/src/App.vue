03797edfb42e3868c467087d70fcbbcfb9d29fbb
Jehan Virot <jehan@fedora-2.home>
2026-04-24 00:03:17 +0200

    environment set up

---
+++ git_handshakes/front/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <button @click="count++">
+      Count is: {{ count }}
+    </button>
+  </div>
+</template>
