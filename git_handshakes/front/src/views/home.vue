ada34b46ec46301ebe103021cba43e3bc3a5af9d
Jehan Virot <jehan@2a01cb08086f7b004bd2b7fa9a1c104b.ipv6.abo.wanadoo.fr>
2026-05-03 04:43:44 +0200

    final commit for today

--- git_handshakes/front/src/views/home.vue
+++ git_handshakes/front/src/views/home.vue
@@ -1,4 +1,14 @@
 <script setup>
+import { ref } from "vue"
 
+const username_github_for_search = ref("")
+const user_github = ref([])
+async function search_git_user(username) {
+    
+    const res = await fetch("http://localhost:3000/search_git_user_info/${username_github_for_search}")
+    const data = await res.json()
+
+    user_github.value = data
+}
 </script>
 
